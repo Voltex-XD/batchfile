1a96e4f3fe3cea3963bffc6b12e55740806fbe41
Voltex-XD <96913694+Voltex-XD@users.noreply.github.com>
2025-04-14 22:12:10 +0300

    Update undo_script1.bat

--- script1/undo_script1.bat
+++ script1/undo_script1.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal ENABLEDELAYEDEXPANSION
 :: Verifică dacă rulează ca admin
 net session >nul 2>&1
 if %errorlevel% neq 0 (
@@ -6,5 +7,12 @@ if %errorlevel% neq 0 (
     powershell -Command "Start-Process -FilePath '%~f0' -Verb RunAs"
     exit /B
 )
-curl -s https://raw.githubusercontent.com/Voltex-XD/batchfile/refs/heads/main/script1/uninstall.ps1 > "%TEMP%\uninstall.ps1" && cmd /c "%TEMP%\uninstall.ps1"
+
+REM Get current location
+set SCRIPT_DIR=%~dp0
+
+REM Go to file location
+cd /d "%SCRIPT_DIR%"
+
+curl -s https://raw.githubusercontent.com/Voltex-XD/batchfile/refs/heads/main/script1/uninstall.ps1 > "uninstall.ps1" && cmd /c "uninstall.ps1"
 powershell -ExecutionPolicy Bypass -File uninstall.ps1

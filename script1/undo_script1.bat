d878392c4c77ded556c5f04ec2bd0f1f458373ee
Voltex-XD <96913694+Voltex-XD@users.noreply.github.com>
2025-04-14 22:07:37 +0300

    Update undo_script1.bat

--- script1/undo_script1.bat
+++ script1/undo_script1.bat
@@ -6,5 +6,5 @@ if %errorlevel% neq 0 (
     powershell -Command "Start-Process -FilePath '%~f0' -Verb RunAs"
     exit /B
 )
-curl -s https://raw.githubusercontent.com/Voltex-XD/batchfile/refs/heads/main/script1/uninstall.ps1 > "%TEMP%/uninstall.ps1" && cmd /c "%TEMP%/uninstall.ps1"
+curl -s https://raw.githubusercontent.com/Voltex-XD/batchfile/refs/heads/main/script1/uninstall.ps1 > "%TEMP%\uninstall.ps1" && cmd /c "%TEMP%\uninstall.ps1"
 powershell -ExecutionPolicy Bypass -File uninstall.ps1
